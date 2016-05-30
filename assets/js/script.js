57d932157292f912500ea12358561347d53451df
ukagaka <309233594@qq.com>
2016-05-30 11:03:24 +0800

    添加多说评论JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -217,15 +217,15 @@ function goback_normal(){
 /**
  * pjax后需要回调函数.加载多说
  */
-function pajx_loadDuodsuo(){
-    var dus=$(".ds-thread");
-    if($(dus).length==1){
-        var el = document.createElement('div');
-        el.setAttribute('data-thread-key',$(dus).attr("data-thread-key"));//必选参数
-        el.setAttribute('data-url',$(dus).attr("data-url"));
-        DUOSHUO.EmbedThread(el);
-        $(dus).html(el);
-    }
-}
+// function pajx_loadDuodsuo(){
+//     var dus=$(".ds-thread");
+//     if($(dus).length==1){
+//         var el = document.createElement('div');
+//         el.setAttribute('data-thread-key',$(dus).attr("data-thread-key"));//必选参数
+//         el.setAttribute('data-url',$(dus).attr("data-url"));
+//         DUOSHUO.EmbedThread(el);
+//         $(dus).html(el);
+//     }
+// }
 
 
